512a599d8f597a151a97993c6e7754d8bce72952
gyfastas <35861032+gyfastas@users.noreply.github.com>
2018-09-30 14:14:02 +0800

    完成作业1

--- HW1/His_specialization.m
+++ HW1/His_specialization.m
@@ -1,19 +1,25 @@
 %read image and convert into grey
-img1 = imread('./image./1.jpg');
-img2 = imread('./image./2.jpg');
+%img1 is the original image
+img1 = imread('./image./9.jpg');
+%img2 is the target image
+img2 = imread('./image./10.jpg');
+
 img1 = rgb2gray(img1);
 img2 = rgb2gray(img2);
 im1 = img1;
 im2 = img2;
-%make histogram
+% histogram
 hist1 = zeros(1,256);
 hist2 = zeros(1,256);
 for i = 1:256
     hist1(i) = sum(sum(img1==i-1));
     hist2(i) = sum(sum(img2==i-1));
 end
+
 hist1 = hist1/sum(hist1);
 hist2 = hist2/sum(hist2);
+
+%cumulative histogram of img1 and img2
 cu_hist1 = cumsum(hist1);
 cu_hist2 = cumsum(hist2);
 I = zeros(1,256);
@@ -24,7 +30,7 @@ for i = 1:256
     img1(find(im1==i-1)) = I(i);
 end
 
-%calculate histogram of changed image
+%calculate histogram of adjusted image
 hist3 = zeros(1,256);
 for k = 1:256
     hist3(k) = sum(sum(img1==k-1));
@@ -34,26 +40,33 @@ hist3 = hist3/sum(hist3);
 
 subplot(331);
 plot(hist1);
+title('Histogram of img1');
 subplot(332);
 plot(cu_hist1);
+title('Cumulative Histogram of img1');
 
 subplot(333);
 imshow(im1);
 
+
 subplot(334);
 plot(hist2);
+title('Histogram of img2');
 
 subplot(335);
 plot(cu_hist2);
+title('Cumulative Histogram of img2');
 
 subplot(336);
 imshow(im2);
 
 subplot(337);
 plot(hist3);
+title('Historgram of adjusted img1');
 
 subplot(338);
 plot(I);
+title('Lookup Table');
 
 subplot(339);
 imshow(img1);
--- HW1/Readme.txt
+++ HW1/Readme.txt
@@ -1,34 +1,53 @@
-# CV_Homework
+CV_Homework
 
-  This is my homework for EE382-Visual localization &Perception
+  
+This is my homework for EE382-Visual localization &Perception
 
-## HW1
+HW1
 
-### Requirement
+
+
+Requirement
+:
+
 
 Write a small program to implement a histogram specialization algorithm and test on 5 pairs of images with different exposure time.
 
-### Environment Required
+
+
+Environment:
+
+
 
 The program is implemented by Matlab. You should have Matlab 2015b+ to run the program.
 
 
 
-### Tutorial for this program
+
+
+Tutorial for this program
+
+:
 
 To run the program, use matlab to open His_specialization.m and click run.
 
+
 To adjust original image and target image, change the direction of img1 and img2 in this program.
 
-```matlab
-%img1 is the original image
-img1 = imread('./image./3.jpg');
-%img2 is the target image
-img2 = imread('./image./4.jpg');
-```
+ 
+ %img1 is the original image
+    
+    img1 = imread('./image./3.jpg');
+    
+ %img2 is the target image
+    
+    img2 = imread('./image./4.jpg');
 
 
 
-The test images are in **image** folder
 
-The test results are in **result** folder
+
+The test images are in image folder
+
+
+The test results are in result folder
